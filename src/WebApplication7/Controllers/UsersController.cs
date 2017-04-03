105dab84c87a136370eaca87b26647186d2fc3cc
mazguhin <mazguhin.evgen@gmail.com>
2017-04-03 14:02:14 +0500

    Edited for Lab10

--- src/WebApplication7/Controllers/UsersController.cs
+++ src/WebApplication7/Controllers/UsersController.cs
@@ -21,6 +21,16 @@ namespace WebApplication7.Controllers
             return db.Users.ToList();
         }
 
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            User user = db.Users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+                return NotFound();
+            return new ObjectResult(user);
+        }
+
         // POST api/users
         [HttpPost]
         public IActionResult Post([FromBody]User user)
@@ -37,6 +47,43 @@ namespace WebApplication7.Controllers
             db.Users.Add(user);
             db.SaveChanges();
             return Ok(user);
-        }        
+        }
+
+        // PUT api/users/
+        [HttpPut]
+        public IActionResult Put([FromBody]User user)
+        {
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Заполните обязательные поля");
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            if (!db.Users.Any(x => x.Id == user.Id))
+            {
+                return NotFound();
+            }
+
+            db.Update(user);
+            db.SaveChanges();
+            return Ok(user);
+        }
+
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            User user = db.Users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return Ok(user);
+        }
     }
 }
--- src/WebApplication7/wwwroot/js/main.js
+++ src/WebApplication7/wwwroot/js/main.js
@@ -78,5 +78,85 @@ viewmodel.create = function () {
         })
 }
 
+viewmodel.editUser = function (userId) {
+    $('#errors').empty();
+    $('#errors').hide();
+    userName = $('#name').val();
+    userAge = $('#age').val();
+    $.ajax({
+        url: "api/users",
+        contentType: "application/json",
+        method: "PUT",
+        data: JSON.stringify({
+            id: userId,
+            name: userName,
+            age: userAge
+        }),
+        success: function (user) {
+            $('#nowId').val(0);
+            viewmodel.reset();
+            viewmodel.update();
+        },
+        error: function (jxqr, error, status) {
+            if (jxqr.responseText === "") {
+                $('#errors').append("<h3>" + jxqr.statusText + "</h3>");
+            }
+            else {
+                var response = JSON.parse(jxqr.responseText);
+                if (response['']) {
+
+                    $.each(response[''], function (index, item) {
+                        $('#errors').append("<p>" + item + "</p>");
+                    });
+                }
+
+                if (response['Name']) {
+
+                    $.each(response['Name'], function (index, item) {
+                        $('#errors').append("<p>" + item + "</p>");
+                    });
+                }
+
+                if (response['Age']) {
+                    $.each(response['Age'], function (index, item) {
+                        $('#errors').append("<p>" + item + "</p>");
+                    });
+                }
+            }
+
+            $('#errors').show();
+        }
+    })
+}
+
+viewmodel.deleteUser = function (id) {
+    $.ajax({
+        url: "api/users/" + id,
+        contentType: "application/json",
+        method: "DELETE",
+        success: function (user) {
+            viewmodel.update();
+        }
+    })
+}
+
+viewmodel.sub = function () {
+    var editUserId = $('#nowId').val()
+    if (editUserId == 0)
+        viewmodel.create();
+    else
+        viewmodel.editUser(editUserId);
+};
+
+viewmodel.edit = function () {
+    $('#nowId').val(($(this)[0])['id']);
+    $('#name').val(($(this)[0])['name']);
+    $('#age').val(($(this)[0])['age']);
+};
+
+viewmodel.delete = function () {
+    viewmodel.deleteUser (($(this)[0])['id']);
+};
+
 ko.applyBindings(viewmodel);
 viewmodel.update();
